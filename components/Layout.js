6cc694f07da730937526a711e11fbc57a0690c8b
NelsonWang <wangnelson2@gmail.com>
2022-02-05 22:36:06 -0500

    events | some design docs

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,20 @@
 import React from "react";
-
 import Header from "./Header";
-
 import Footer from "./Footer";
+import Head from 'next/head';
 
 export default function LayoutOne(props) {
     return (
         <>
-            <head>
+            <Head>
+                <link rel="icon" href="slc-icon32x32.ico" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>Koppelman Student Leadership Council</title>
                 <meta name="keywords" content="slc, brooklyn, brooklyn college, cuny, Koppelman student leader council, brooklyn student leadership council, brooklyn college business, brooklyn college leadership, koppelman school of business" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="description" content="The SLC aims to inspire and encourage students to take the lead in their personal and professional lives. We aim to ensure students are equipped with the resources and knowledge they need to dive into their professional careers." />
                 <meta name="author" content="Koppelman Student Leadership Council" />
-            </head>
+            </Head>
             <body className="pb-5" >
 
                 <Header />
