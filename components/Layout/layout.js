64a54cc009d2686049a09daf1ac77a88b973274c
NelsonWang <wangnelson2@gmail.com>
2022-02-05 19:51:10 -0500

    layout | basic

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+
+export default function LayoutOne(props){
+    return(
+        <>
+        <Head>
+            <title>Koppelman Student Leadership Council</title>
+        </Head>
+        {props.children}
+        </>
+    )
+}
