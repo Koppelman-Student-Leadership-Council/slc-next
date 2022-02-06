5f88b509f63ac1ad02e6f895d0d52f69a176ed9d
NelsonWang <wangnelson2@gmail.com>
2022-02-06 01:39:02 -0500

    edit | sideabouttitle | siders

--- components/sider-about-title.js
+++ components/sider-about-title.js
@@ -3,13 +3,13 @@ import PostSubTitle from "./post-sub-title";
 
 export default function sidedAbout({ content, image, title }) {
     return (<>
-        <div class="row shadow-big hover:shadow-medium transition-shadow duration-200">
+        <div class="row">
+            <div className="col-md-4 justify-content-center align-self-center ">
+                <img src={image} class="height-250" alt={title} />
+            </div>
             <div className="col-md-8 justify-content-center align-self-center">
                 <h2 class="text-1xl md:text-2xl lg:text-3xl">{title}</h2>
                 <p>{content}</p>
-            </div>
-            <div className="col-md-4 justify-content-center align-self-center ">
-                <img src={image} class="height-250" alt={title} />
             </div>
         </div>
     </>);
--- components/sider-about.js
+++ components/sider-about.js
@@ -1,6 +1,6 @@
 export default function sidedAbout({ content, image }) {
     return (<>   <div class="container">
-        <div class="row">
+        <div class="row shadow-big hover:shadow-medium transition-shadow duration-200">
             <div class="col-md-4" data-aos="fade-down-right" data-aos-duration="1500">
                 <img class="height-250" src={image} />
             </div>
