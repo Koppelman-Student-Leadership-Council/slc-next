62d49d48ca7931526520e780561c3b379562d7ab
NelsonWang <wangnelson2@gmail.com>
2022-02-05 23:58:28 -0500

    edit | bar down

--- components/post-title.js
+++ components/post-title.js
@@ -9,7 +9,7 @@ export default function PostTitle({ children, breadcrumb }) {
         <br />
         <br />
         {breadcrumb && <Breadcrumbs />}
-        <h1 className=" closer-top text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 ml-2">
+        <h1 className="link link--metis closer-top text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 ml-2">
           {children}
         </h1>
        
