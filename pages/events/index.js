daa6d4c33e8e20088298f3a1614161ec7cf1c0fa
NelsonWang <wangnelson2@gmail.com>
2022-02-05 21:12:49 -0500

    events | learn more

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,15 @@
 import Layout from "../../components/Layout";
-import PostTitle from  "../../components/post-title";
+import PostTitle from "../../components/post-title";
+import ProgramsPreview from '../../components/program-preview'
 
 function HomePage() {
     return <>
-       <Layout>
-           <PostTitle breadcrumb>Events</PostTitle>
-        <div className="">
-        </div>
-       </Layout>
+        <Layout>
+            <PostTitle breadcrumb>Events</PostTitle>
+            <div className="">
+                <ProgramsPreview title="Learn More" slug="learn-more" excerpt="Learn more about slc." />
+            </div>
+        </Layout>
     </>
 }
 
