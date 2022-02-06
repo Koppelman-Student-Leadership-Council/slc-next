5f1bed970ff89dfc59eb180cab5811a2ab456338
NelsonWang <wangnelson2@gmail.com>
2022-02-05 20:01:46 -0500

    layout | meta

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 
-export default function LayoutOne(props){
-    return(
+export default function LayoutOne(props) {
+    return (
         <>
-        <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Koppelman Student Leadership Council</title>
-        </Head>
-        {props.children}
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Koppelman Student Leadership Council</title>
+                <meta name="keywords" content="slc, brooklyn, brooklyn college, cuny, Koppelman student leader council, brooklyn student leadership council, brooklyn college business, brooklyn college leadership, koppelman school of business" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="description" content="The SLC aims to inspire and encourage students to take the lead in their personal and professional lives. We aim to ensure students are equipped with the resources and knowledge they need to dive into their professional careers." />
+                <meta name="author" content="Koppelman Student Leadership Council" />
+            </Head>
+            {props.children}
         </>
     )
 }
