25aeabaa0061247cf57376e2b062b115b44a82c8
NelsonWang <wangnelson2@gmail.com>
2022-02-05 20:42:02 -0500

    layout | footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,29 @@
 import React from "react";
 
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function LayoutOne(props) {
     return (
         <>
-            <Head>
+            <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>Koppelman Student Leadership Council</title>
                 <meta name="keywords" content="slc, brooklyn, brooklyn college, cuny, Koppelman student leader council, brooklyn student leadership council, brooklyn college business, brooklyn college leadership, koppelman school of business" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="description" content="The SLC aims to inspire and encourage students to take the lead in their personal and professional lives. We aim to ensure students are equipped with the resources and knowledge they need to dive into their professional careers." />
                 <meta name="author" content="Koppelman Student Leadership Council" />
-            </Head>
+            </head>
             <body>
 
-                <div className="container-slc">
+                <div className="container-slc container">
+                    <Header/>
                     {props.children}
+                    <Footer/>
                 </div>
             </body>
+
+
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Header from "../components/Header";
+import Layout from "../components/Layout";
 
 function HomePage() {
   return <>
-  <Header/>
+  <Layout>
+    
+  </Layout>
   </>
 }
 
