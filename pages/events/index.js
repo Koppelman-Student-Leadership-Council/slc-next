07426749b5d2212c88685f462cf43665c9ad1c98
NelsonWang <wangnelson2@gmail.com>
2022-02-05 21:21:23 -0500

    events | section

--- pages/events/index.js
+++ pages/events/index.js
@@ -6,9 +6,13 @@ function HomePage() {
     return <>
         <Layout>
             <PostTitle breadcrumb>Events</PostTitle>
-            <div className="">
-                <EventPreview title="Learn More" slug="learn-more" excerpt="Learn more about slc." />
-            </div>
+            <section>
+                <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
+                    <EventPreview title="Business Matters" slug="bussiness-matters" excerpt="Learn more about slc Business matters." />
+                    <EventPreview title="KSB Summit" slug="summit" excerpt="Learn more about slc KSB Summit." />
+                    <EventPreview title="Learn More" slug="learn-more" excerpt="Learn more about slc." />
+                </div>
+            </section>
         </Layout>
     </>
 }
