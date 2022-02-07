f5c047dcd0c9dbad1625cc85e6f9c2db421acf73
NelsonWang <wangnelson2@gmail.com>
2022-02-07 13:57:49 -0500

    calendar | clubs | maps href

--- components/community-preview.js
+++ components/community-preview.js
@@ -10,14 +10,14 @@ export default function PostPreview({
   return (
     <div className="p-3 shadow-big hover:shadow-medium transition-shadow duration-200" >
       <div className="mb-5 ">
-      <Link as={`/community/${slug}`} href="/community/[slug]">
+      <Link as={`/community/${slug}`} href={`/community/${slug}`}>
         <a>
         <CoverImage slug={slug} title={title} src={coverImage} postUrl={"programs"}  />
         </a>
         </Link>
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/community/${slug}`} href="/community/[slug]">
+        <Link as={`/community/${slug}`} href={`/community/${slug}`}>
           <a className="text-decoration-none text-secondary">{title}</a>
         </Link>
       </h3>
