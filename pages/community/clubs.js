d165f641e8ebf99406f45ab732555aa4f89ec3b4
NelsonWang <wangnelson2@gmail.com>
2022-02-07 13:11:08 -0500

    calendar | clubs

--- pages/community/clubs.js
+++ pages/community/clubs.js
@@ -2,12 +2,17 @@ import Layout from "../../components/Layout";
 import PostTitle from "../../components/post-title";
 import Partnership from "../../components/partnership";
 
+import Clubs from '../../data/clubs.json';
+
 function HomePage() {
+    console.log(Clubs);
     return <>
         <Layout>
             <PostTitle breadcrumb>Our Clubs</PostTitle>
             <div className="row">
-                <Partnership />
+                {Clubs.map((club)=>{
+                   return  <Partnership title={club["title"]} link={club['link']} imageLink={"/assets/clubs/resized/"+club['imagename']+".png"} />
+                })}
             </div>
         </Layout>
     </>
