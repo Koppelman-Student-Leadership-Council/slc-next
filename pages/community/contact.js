45c3b4ec7dcd2893f46007bb42170d235640679a
NelsonWang <wangnelson2@gmail.com>
2022-02-07 20:50:00 -0500

    calendar | clubs | contact

--- pages/community/contact.js
+++ pages/community/contact.js
@@ -3,40 +3,56 @@ import PostTitle from "../../components/post-title";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Map from '../../components/map'
+import emailjs from 'emailjs-com';
+import axios from "axios";
+// SLADmin448!12334
+// wangnelson2@gmail.com
 
-function HomePage() {
+function Contact() {
+
+    const handleSubmission = async e => {
+        event.preventDefault()
+    
+        emailjs.sendForm('service_3wgvjnc', 'template_cgpz9g9', e.target, 'user_bUjdCRqLeJSakqKuFlLB7')
+        .then((result) => {
+            window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+        }, (error) => {
+            console.log(error.text);
+        });
+    
+    }
 
     const mapUrl = "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAHedcWWJ17w-V1js7ERyz3USvPosmjNKQ";
     return <>
         <Layout>
-            <PostTitle breadcrumb>Contact</PostTitle>
+            <PostTitle breadcrumb >Contact</PostTitle>
             <p>
                 Complete the form below or email us at <b>brooklynslcouncil@gmail.com</b>
             </p>
 
-            <Form>
+            <Form onSubmit={handleSubmission} action="https://brooklynslcouncil.com/mail/contact_me.php">
 
                 <Form.Group>
                     <Form.Label>Name</Form.Label>
-                    <Form.Control required className="dark-focus" type="text" placeholder="John Doe" />
+                    <Form.Control id="name" required className="dark-focus" type="text" placeholder="John Doe" />
                 </Form.Group>
                 <br />
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control required className="dark-focus" type="email" placeholder="johndoe@gmail.com" />
+                    <Form.Label  >Email address</Form.Label>
+                    <Form.Control id="email" required className="dark-focus" type="email" placeholder="johndoe@gmail.com" />
 
                 </Form.Group>
                 <br />
                 <Form.Group>
-                    <Form.Label>Phone Number</Form.Label>
+                    <Form.Label >Phone Number</Form.Label>
 
-                    <Form.Control required className="dark-focus" type="number" placeholder="Interview Request" />
+                    <Form.Control id="phone"  required className="dark-focus" type="number" placeholder="Interview Request" />
 
                 </Form.Group>
                 <br />
                 <Form.Group>
-                    <Form.Label>Message</Form.Label>
-                    <Form.Control className="dark-focus" as="textarea" rows={3} placeholder="Message." />
+                    <Form.Label  >Message</Form.Label>
+                    <Form.Control id="message" className="dark-focus" as="textarea" rows={3} placeholder="Message." />
                 </Form.Group>
                 <br />
                 <Button type="submit" className="black-text" >
@@ -64,4 +80,4 @@ function HomePage() {
     </>
 }
 
-export default HomePage
+export default Contact
