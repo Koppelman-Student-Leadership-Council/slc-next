afdd453c589dfca9d1b5c0b75941ec6249486e2a
NelsonWang <wangnelson2@gmail.com>
2022-02-07 23:13:09 -0500

    Production update |

--- pages/events/learn-more.js
+++ pages/events/learn-more.js
@@ -6,8 +6,8 @@ function HomePage() {
         <Layout>
             <PostTitle breadcrumb>Learn More</PostTitle>
             <div className="">
-                <p>Leadership
-The Student Leadership Council aims to cultivate the leadership potential of all our members. We offer a unique platform for idea-sharing and development, access to senior administrators in the KSB, and encourage peer-to-peer mentoring.We encourage our members to strive for more in their careers, academics, and in their communities.</p>
+                <p>
+                    The Student Leadership Council aims to cultivate the leadership potential of all our members. We offer a unique platform for idea-sharing and development, access to senior administrators in the KSB, and encourage peer-to-peer mentoring.We encourage our members to strive for more in their careers, academics, and in their communities.</p>
             </div>
         </Layout>
     </>
