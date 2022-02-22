a23eb67b9bd5a89c60dacdeaf11c71a03bfa1156
NelsonWang <wangnelson2@gmail.com>
2022-02-21 19:27:05 -0500

    Production update |

--- components/Layout.js
+++ components/Layout.js
@@ -21,9 +21,7 @@ export default function LayoutOne(props) {
                     {props.children}
                 </div>
             </body>  <script src="https://cdnjs.cloudflare.com/ajax/libs/react-modal/3.14.3/react-modal.min.js"
-                integrity="sha512-MY2jfK3DBnVzdS2V8MXo5lRtr0mNRroUI9hoLVv2/yL3vrJTam3VzASuKQ96fLEpyYIT4a8o7YgtUs5lPjiLVQ=="
-                crossorigin="anonymous"
-                referrerpolicy="no-referrer"></script>
+               ></script>
             <Footer />
 
 
