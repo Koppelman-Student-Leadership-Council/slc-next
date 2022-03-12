bede28b6778ee5328ce83af8caf6f58d4686aee2
Eric Ma <ma.eric11214@outlook.com>
2022-03-11 21:06:37 -0500

    community index adjustments

--- components/community-preview.js
+++ components/community-preview.js
@@ -8,7 +8,7 @@ export default function PostPreview({
   slug,
 }) {
   return (
-    <div className="p-3 shadow-big hover:shadow-medium transition-shadow duration-200" >
+    <div className="p-3 shadow-big hover:shadow-medium transition-shadow duration-200 rounded-10">
       <div className="mb-5 ">
       <Link as={`/community/${slug}`} href={`/community/${slug}`}>
         <a>
@@ -16,12 +16,12 @@ export default function PostPreview({
         </a>
         </Link>
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className="pl-4 text-3xl mb-2 leading-snug">
         <Link as={`/community/${slug}`} href={`/community/${slug}`}>
           <a className="text-decoration-none text-secondary">{title}</a>
         </Link>
       </h3>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+      <p className="text-lg leading-relaxed mb-4 pl-4">{excerpt}</p>
     </div>
   )
 }
