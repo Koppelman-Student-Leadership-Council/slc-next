d8d9e942d15518fbede568815236be656cf81d26
NelsonWang <wangnelson2@gmail.com>
2022-03-25 09:53:36 -0400

    images | index | looking weird fix | both sides

--- components/sidel-about-title.js
+++ components/sidel-about-title.js
@@ -3,13 +3,13 @@ import PostSubTitle from "./post-sub-title";
 
 export default function sidedAbout({ content, image, title }) {
     return (<>
-        <div className="group grid grid-cols-3 shadow-big hover:shadow-medium transition-shadow duration-200 rounded-10">
-            <div className="col-md-8 align-self-center justify-self-start w-auto col-span-2 pl-7 pr-10">
+        <div className="group grid grid-cols-1 sm:grid-cols-3 shadow-big hover:shadow-medium transition-shadow duration-200 rounded-10">
+            <div className="col-span-2 align-self-center justify-self-start w-auto  pl-7 pr-10">
                 <h2 className="text-2xl lg:text-3xl">{title}</h2>
                 <p>{content}</p>
             </div>
-            {image && <div className="col-md-4 align-self-center w-auto justify-self-end group-hover:scale-105 transition-transform duration-200">
-                <img src={image} className="h-[250px] rounded-10" alt={title} />
+            {image && <div className="align-self-center w-auto justify-self-end group-hover:scale-105 transition-transform duration-200">
+                <img src={image} className="h-[300] rounded-10" alt={title} />
             </div>}
             
         </div>
--- components/sider-about-title.js
+++ components/sider-about-title.js
@@ -4,8 +4,8 @@ import PostSubTitle from "./post-sub-title";
 export default function sidedAbout({ content, image, title }) {
     return (<>
         <div className="group grid grid-cols-1 sm:grid-cols-3 hover:shadow-medium transition-shadow duration-200 rounded-10">
-            {image && <div className=" ">
-                <img src={image} className=" max-h-[300px] rounded-10 group-hover:scale-105 transition-transform duration-200" alt={title} />
+            {image && <div className=" align-self-center w-auto justify-self-end group-hover:scale-105 transition-transform duration-200">
+                <img src={image} className="h-[300] rounded-10" alt={title} />
             </div>}
             <div className="col-span-2 align-self-center w-auto justify-self-end pr-7 pl-10">
                 <h2 className="text-2xl lg:text-3xl">{title}</h2>
