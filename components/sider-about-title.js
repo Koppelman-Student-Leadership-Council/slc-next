b218d2823140d275f7523104101e925fb1124d7b
Eric Ma <ma.eric11214@outlook.com>
2022-04-12 19:20:55 -0400

    fixed right side on landing page

--- components/sider-about-title.js
+++ components/sider-about-title.js
@@ -11,7 +11,7 @@ export default function sidedAbout({ content, image, title }) {
             {image && <div className="align-self-center w-[24rem] -mr-8 justify-self-end transition-transform duration-200 md:group-hover:scale-105 md:ml-0 md:w-auto">
                 <img src={image} className="h-[300] rounded-10" alt={title} />
             </div>}
-            <div className="col-span-2 align-self-center w-auto justify-self-end pr-7 pl-10 -mx-16">
+            <div className="col-span-2 align-self-center w-auto justify-self-end pr-7 pl-10 -ml-16">
                 <h2 className="text-3xl lg:text-3xl pt-3 pb-2">{title}</h2>
                 <p>{content}</p>
             </div></div>
