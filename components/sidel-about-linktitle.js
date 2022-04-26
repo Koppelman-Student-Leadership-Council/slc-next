1439689b28b2072b55258819a9c534d56406e8db
NelsonWang <wangnelson2@gmail.com>
2022-04-26 00:21:38 -0400

    travel tools | sidelabout title

--- components/sidel-about-linktitle.js
+++ components/sidel-about-linktitle.js
@@ -3,20 +3,18 @@ import PostSubTitle from "./post-sub-title";
 
 export default function sidedAbout({ content, image, title }) {
     return (<>
-        <div className={"group grid grid-cols-1 transition-shadow duration-200 rounded-10 text-lg " + 
-        "md:grid-cols-3 lg:hover:shadow-[-45px_55px_30px_-45px] " + 
-        (title == "Leadership" || title == "Community" ? "lg:hover:shadow-[#AD4C66] " : title == "Build Skills" ? "lg:hover:shadow-[#ebba13] " : " ")
+        <div className={"group grid grid-cols-1 transition-shadow duration-200 rounded-10 text-lg " +
+            "md:grid-cols-3 lg:hover:shadow-[-45px_55px_30px_-45px] "
+
         }>
             <div className="col-span-2 align-self-center w-auto pl-7 pr-10 -mx-2 order-3 md:mx-0 text-right">
                 <h2 className="text-3xl pt-3 pb-2 md:pt-0 md:pb-0">{title}</h2>
                 <p>{content}</p>
             </div>
-            {image && <div className={"align-self-center justify-self-center rounded-10 w-[90vw] transition-all duration-200 -order-1 md:w-auto md:order-3 lg:group-hover:scale-105 " +
-            (title == "Leadership" || title == "Community" ? "lg:group-hover:shadow-[#ebba13] lg:group-hover:shadow-[20px_-20px_25px_-15px] " : title == "Build Skills" ? "lg:group-hover:shadow-[#AD4C66] lg:group-hover:shadow-[20px_-20px_25px_-15px] " : " ")
-            }>
+            {image && <div className={"align-self-center justify-self-center rounded-10 w-[90vw] transition-all duration-200 -order-1 md:w-auto md:order-3 lg:group-hover:scale-105 "}>
                 <img src={image} className="h-[300] rounded-10" alt={title} />
             </div>}
-            
+
         </div>
     </>);
 }
--- pages/about/tools.js
+++ pages/about/tools.js
@@ -2,49 +2,29 @@ import Layout from "../../components/Layout";
 import PostTitle from "../../components/post-title";
 import FullDetailAbout from "../../components/full-detail-about"
 import SideRightAboutTitle from "../../components/sider-about-title"
+import sideLAboutTitle from "../../components/sidel-about-linktitle"
 
 
-const departments=[
-    "Operations","Alumni Relations", "Club & Faculty Relations", "Technology", "Marketing"
-]
 
-const departmentData = {
-    "Operations": "The Operations Department is responsible for creating the highest level of efficiency possible within the SLC. As a Department, we directly assist the Executive Team with daily operations, including scheduling, close monitoring of deadlines, and task delegation. The Operations Department is responsible for recording impact-focused minutes and key accomplishments/decisions made during the meetings. We aim to ensure that all operations processes when organizing an event are carried out most appropriately. Some of those processes are: creating a day of task and event structures for signature events, reserving space and equipment for events and meetings, and managing zoom events.",
-    "Alumni Relations": "The Alumni Relations Department is responsible for fostering relationships with SLC’s " +
-        "alumni network and creating an engaged and supportive connection between former and current SLC members. " +
-        "SLC’s alumni network consists of diverse members in terms of experience, industries and exposure. The department  " +
-        "is responsible for keeping the alumni aware about the successes and current events hosted by SLC through newsletters.  " +
-        "Buddy System is a mentorship program within SLC that pairs a current member with an alumni for the professional " +
-        " development of the current members and for the alumni to stay better connected and engaged with the progress of SLC",
-    "Club & Faculty Relations": "The Club Relations department aims to act as an intermediary between all of the active KSB  " +
-        "clubs. This department takes great pride in developing intimate connections with clubs in order to establish a larger  " +
-        "network where those clubs can then interact and collaborate with each other. Our chief purpose is to be the bridge that  " +
-        "tethers to all KSB clubs and brings them closer together!",
-    "Technology": "In the Tech department, students from technical backgrounds get to connect with the business students. " +
-        " Over the past few years, CS and CIS students have been heavily envolved in the council.  " +
-        "They have created the official website of the SLC and are currently maintaining it. Apart from these, the tech department  " +
-        "plays a key role helping members of the council with tech support and helps to create a bridge between the tech and business students.",
-    "Marketing": "The marketing department is intended to use creativity to enhance the efforts of our SLC peers by  " +
-        "promoting and advertising events on our social media. Behind the scenes, the department is responsible for creating content,  " +
-        "making flyers, interacting with KBS students online and being the brand ambassadors for SLC. There’s always something " +
-        " new to do in marketing, with creativity and invitation the marketing team is always searching for new ways to improve and advance " +
-        " the student outreach strategy.",
-    "Executive Team": " Their responsibility is to work with the team to create a carefully selected plan for the upcoming semester to promote community involvement and professional development on campus! Here at the council, we work intrinsically to grow our team’s leadership skills and develop their soft skills to set them up for a successful career!"
-}
+const toolsData = [{ title: "titleX", description: "Description" }];
+
 
 function HomePage() {
+
+    const listTools = toolsData.map(item => {
+        return <>
+            <br /><sideLAboutTitle title={item.title} content={item.description} />
+        </>
+    })
+
     return <>
         <Layout>
-            <PostTitle breadcrumb>Departments</PostTitle>
+            <PostTitle breadcrumb>Tools</PostTitle>
             <div className="pt-10 text-lg px-2">
                 <p>
-                    The Student Leadership Council aims to cultivate the leadership potential of all our members. We offer a unique platform for idea-sharing and development, access to senior administrators in the KSB, and encourage peer-to-peer mentoring.We encourage our members to strive for more in their careers, academics, and in their communities.</p>
-            <br /><FullDetailAbout title={departments[0]}  content={departmentData[departments[0]]} />
-            <br /><FullDetailAbout title={departments[1]}  content={departmentData[departments[1]]} />
-            <br /><FullDetailAbout title={departments[2]}  content={departmentData[departments[2]]} />
-            <br /><FullDetailAbout title={departments[3]}  content={departmentData[departments[3]]} />
-            <br /><FullDetailAbout title={departments[4]}  content={departmentData[departments[4]]} />
-            <br /><FullDetailAbout title={departments[5]}  content={departmentData[departments[5]]} />
+                    The Student Leadership Council (SLC) is a student-led organization that aims to create a collaborative environment for students to share their skills and talents. We encourage the sharing of tools and resources  The Tech Department is an extension of SLC, and as such, they are commited to create tools for students to use for free with no intention of getting any revenue from it!</p>
+                <p></p>
+                {listTools}
             </div>
         </Layout>
     </>
