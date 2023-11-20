0c8fd19ebbc8fbf395d1aa955b411861e67eea71
NelsonWang <wangnelson2@gmail.com>
2023-11-20 03:04:37 -0300

    team clubs

--- pages/community/clubs.js
+++ pages/community/clubs.js
@@ -1,30 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import Layout from "../../components/Layout";
 import PostTitle from "../../components/post-title";
 import Partnership from "../../components/partnership";
 
-function HomePage({clubsData}) {
+function HomePage({ initialClubsData }) {
+    const [clubsData, setClubsData] = useState(initialClubsData);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await fetch('https://admin.brooklynslcouncil.com/public/api/clubs');
+                const data = await res.json();
+                setClubsData(data);
+            } catch (error) {
+                console.error("Failed to fetch clubs data:", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
     console.log(clubsData);
-    return <>
+
+    return (
         <Layout>
             <PostTitle breadcrumb>Our Clubs</PostTitle>
             <div className="grid justify-items-center pt-10 pb-28 gap-y-8 sm:grid-cols-2 md:gap-y-10 lg:grid-cols-3 xl:grid-cols-4">
-                {clubsData.map((club)=>{
-                   return  <Partnership key={club["image_link"]} title={club["title"]} link={club['link']} imageLink={club["image_link"]} />
-                })}
+                {clubsData.map((club) => (
+                    <Partnership key={club.image_link_overwrite ?? club.image_link} 
+                                 title={club.title} 
+                                 link={club.link} 
+                                 imageLink={club.image_link} />
+                ))}
             </div>
         </Layout>
-    </>
+    );
 }
 
 export async function getStaticProps() {
-    const res = await fetch('https://admin.brooklynslcouncil.com/public/api/clubs')
-    const clubsData = await res.json()
+    const res = await fetch('https://admin.brooklynslcouncil.com/public/api/clubs');
+    const initialClubsData = await res.json();
     return {
         props: {
-            clubsData,
+            initialClubsData,
         },
     }
 }
 
-
-export default HomePage
+export default HomePage;
--- pages/community/team.js
+++ pages/community/team.js
@@ -1,30 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import Layout from "../../components/Layout";
 import PostTitle from "../../components/post-title";
 import MemberCard from '../../components/MemberCard';
 
-function HomePage({ membersData }) {
-    console.log(membersData)
-    return <>
+function HomePage({ initialMembersData }) {
+    const [membersData, setMembersData] = useState(initialMembersData);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await fetch('https://admin.brooklynslcouncil.com/public/api/team/active');
+                const data = await res.json();
+                setMembersData(data);
+            } catch (error) {
+                console.error("Failed to fetch members data:", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    console.log(membersData);
+
+    return (
         <Layout>
             <PostTitle breadcrumb>Our Team</PostTitle>
             <div className="grid justify-items-center pt-10 px-0 sm:mx-30 lg:px-0 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-10">
-                {membersData.map((member) => {
-                    return <div key={member.name} className="mt-1 mb-1">
-                        <MemberCard image={member.image_link} name={member.name} description={member.description} title={member.title} department={member.department} linkedin={member.linkedin} /></div>
-                })}
+                {membersData.map((member) => (
+                    <div key={member.name} className="mt-1 mb-1">
+                        <MemberCard image={member.image_team_overwrite ?? member.image_link}
+                         name={member.name} description={member.description} title={member.title} department={member.department} linkedin={member.linkedin} />
+                    </div>
+                ))}
             </div>
         </Layout>
-    </>
+    );
 }
 
 export async function getStaticProps() {
-    const res = await fetch('https://admin.brooklynslcouncil.com/public/api/team/active')
-    const membersData = await res.json()
+    const res = await fetch('https://admin.brooklynslcouncil.com/public/api/team/active');
+    const initialMembersData = await res.json();
     return {
         props: {
-            membersData,
+            initialMembersData,
         },
     }
 }
 
-export default HomePage
+export default HomePage;
